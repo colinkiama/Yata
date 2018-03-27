3eb2392e5ed2a8c249658e01b05b45eb255ac09f
Colin Kiama <colinkiama@hotmail.co.uk>
2018-03-27 21:18:31 +0100

    You have all the time in the world to code

--- YATA/Services/TileService2.cs
+++ YATA/Services/TileService2.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace YATA.Services
 {
     public partial class TileService
     {
-        
+        const string serviceSettingsKey = "tileServiceEnabled";
+        public void ChangeTileServiceAvailability()
+        {
+            bool tileServiceEnabled = CheckIfServiceAvailable();
+            if (tileServiceEnabled)
+            {
+                setServiceAvailability(!tileServiceEnabled);
+            }
+        }
+
+        private void setServiceAvailability(bool availability)
+        {
+            var localSettings = ApplicationData.Current.LocalSettings.Values;
+            localSettings[serviceSettingsKey] = availability;
+        }
+
+        private bool CheckIfServiceAvailable()
+        {
+            var localSettings = ApplicationData.Current.LocalSettings.Values;
+            bool isTileServiceEnabled = (bool)localSettings[serviceSettingsKey];
+            return isTileServiceEnabled;
+        }
     }
 }
